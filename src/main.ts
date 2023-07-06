73c8f812a268742b251cbde08f0eef3f34cebd3b
Ángel De La Cruz <angeldelacruzdev@gmail.com>
2023-07-06 15:08:10 -0400

    Tarea - Refactorizar funciones

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import './style.css'
+import "./style.css";
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+import "./clean-code/04-homework";
+
+const app = document.querySelector<HTMLDivElement>("#app")!;
 
 app.innerHTML = `
   <h1>CleanCode y SOLID</h1>
   <span>Revisar la consola de JavaScript</span>
-`
-
+`;
